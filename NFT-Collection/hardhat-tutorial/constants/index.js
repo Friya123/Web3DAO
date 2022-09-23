ff9e98ee84d54a179d2be76057d7e5006f3e0ecc
Friya123 <32993909+Friya123@users.noreply.github.com>
2022-09-23 17:21:23 +0530

    Update index.js

--- NFT-Collection/hardhat-tutorial/constants/index.js
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x6F39D2ac50Ce86878b12318c1ff22561e9fc9f48";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://web3dao-my-app-z2xb.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
